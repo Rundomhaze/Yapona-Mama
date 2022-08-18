6ca14005d40f51c78ab4f2d9f19eb36b0c621c10
Alex <petenkov2822@gmail.com>
2022-08-18 19:05:32 +0300

    auth form on rtk front add

--- frontend/src/features/navbar/Navbar.jsx
+++ frontend/src/features/navbar/Navbar.jsx
@@ -3,27 +3,40 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className="navbar">
+    <>
+      <div className="divnavbar">
+        <nav>
+          <div className="nav-wrapper sticky-nav">
+            <a href="/" className="brand-logo">Logo</a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><a href="sass.html">Меню</a></li>
+              <li><a href="sass.html">Войти</a></li>
+              <li><a href="sass.html">Регистрация</a></li>
+              <li><a href="badges.html">Акции</a></li>
+              <li><a href="collapsible.html">Корзина</a></li>
+              <li><a href="mobile.html">Доставка</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul className="sidenav" id="mobile-demo">
+          <li><a href="sass.html">Меню</a></li>
+          <li><a href="badges.html">Акции</a></li>
+          <li><a href="collapsible.html">Корзина</a></li>
+          <li><a href="mobile.html">Доставка</a></li>
+        </ul>
+      </div>
       <nav>
-        <div class="nav-wrapper sticky-nav">
-          <a href="/" class="brand-logo">Logo</a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="sass.html">Меню</a></li>
-            <li><a href="badges.html">Акции</a></li>
-            <li><a href="collapsible.html">Корзина</a></li>
-            <li><a href="mobile.html">Доставка</a></li>
+        <div className="divnavbar2">
+          <ul id="nav-mobile" className="">
+            <li><a>Работаем 11:00 - 04:00 </a></li>
+            <li><a>Доставка еды от 45 минут</a></li>
+            <li><a href="collapsible.html">Личный кабинет</a></li>
           </ul>
         </div>
       </nav>
-
-      <ul class="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Меню</a></li>
-        <li><a href="badges.html">Акции</a></li>
-        <li><a href="collapsible.html">Корзина</a></li>
-        <li><a href="mobile.html">Доставка</a></li>
-      </ul>
-    </div>
+    </>
   )
 }
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {store} from './app/store';
+import { Provider } from 'react-redux';
 import App from './features/main/App';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 

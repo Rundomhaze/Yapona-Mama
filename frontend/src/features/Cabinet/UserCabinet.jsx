a9d0230ff2d991df13290ff4ffc0d0d2186441c6
Alex <petenkov2822@gmail.com>
2022-08-24 14:15:19 +0300

    cabinet user edit info & password done

--- frontend/src/features/Cabinet/UserCabinet.jsx
+++ frontend/src/features/Cabinet/UserCabinet.jsx
@@ -1,21 +1,53 @@
-import React, {useEffect} from "react";
+import React, { useState } from "react";
 import './UserCabinet.css';
 import InputMask from "react-input-mask";
 import { useSelector } from 'react-redux';
 
 function UserCabinet() {
   const { user } = useSelector((state) => state.user);
-  console.log(user);
+  const [number, setNumber] = useState(null)
+  const [editInfoMessage, setEditInfoMessage] = useState(null)
+  const [editPassMessage, setEditPassMessage] = useState(null);
 
-
-  function handleSubmitInfo() {
-
+  async function handleSubmitInfo(e) {
+    e.preventDefault();
+    const userInfo = {
+      id: user.id,
+      name: e.target.name.value,
+      email: e.target.email.value,
+      phone: e.target.tel.value,
+      orderStreet: e.target.orderStreet.value,
+      orderHouse: e.target.orderHouse.value,
+      orderEntrance: e.target.orderEntrance.value,
+      orderFloor: e.target.orderFloor.value,
+      orderFlat: e.target.orderFlat.value
+    }
+    const result = await fetch('/api/edit_user_info', {
+      method: 'POST',
+      body: JSON.stringify(userInfo),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const message = await result.json()
+    setEditInfoMessage(message)
   }
 
-  function handleSubmitPassword() {
-
+  async function handleSubmitPassword(e) {
+    e.preventDefault();
+    const newPassword = {
+      id: user.id,
+      password: e.target.password.value,
+      passwordConfirm: e.target.passwordRepeat.value,
+    }
+    const result = await fetch('/api/edit_user_pass', {
+      method: 'POST',
+      body: JSON.stringify(newPassword),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const message = await result.json()
+    setEditPassMessage(message)
   }
 
+  const userPhone = user && user.phone;
   return (
     <>
       <h3>Личный кабинет</h3>
@@ -38,61 +70,74 @@ function UserCabinet() {
             defaultValue={user.email} />
 
           <label>Телефон</label>
+
           <InputMask
             mask="8-(999)-999-99-99"
             name='tel'
             type="tel"
             className='modal_input'
-            value={user.phone} />
+            onChange={(e) => setNumber(e.target.value)}
+            value={number || user.phone}
+          />
 
           <label>Улица</label>
           <input
-            name=''
+            name='orderStreet'
             type="text"
-            className='modal_input' />
+            className='modal_input'
+            defaultValue={user.orderStreet} />
 
           <label>Дом</label>
           <input
-            name=''
+            name='orderHouse'
             type="text"
-            className='modal_input' />
+            className='modal_input'
+            defaultValue={user.orderHouse} />
 
           <label>Парадная</label>
           <input
-            name=''
+            name='orderEntrance'
             type="text"
-            className='modal_input' />
+            className='modal_input'
+            defaultValue={user.orderEntrance} />
 
           <label>Этаж</label>
           <input
-            name=''
+            name='orderFloor'
             type="text"
-            className='modal_input' />
+            className='modal_input'
+            defaultValue={user.orderFloor} />
 
           <label>Квартира</label>
           <input
-            name=''
+            name='orderFlat'
             type="text"
-            className='modal_input' />
+            className='modal_input'
+            defaultValue={user.orderFlat} />
+
+          {editInfoMessage && (<p>{editInfoMessage.message}</p>)}
 
           <button type='submit' className='btn modal_button' >сохранить</button>
         </form>
 
         <form className="formPass" onSubmit={handleSubmitPassword}>
-        <h5 className='title'>Изменить пароль</h5><br />
+          <h5 className='title'>Изменить пароль</h5><br />
           <label>Пароль</label>
           <input
-            name=''
+            name='password'
             type="password"
             className='modal_input'
-            placeholder="новый пароль"  />
+            placeholder="новый пароль" />
 
           <label>Повторите пароль</label>
           <input
-            name=''
+            name='passwordRepeat'
             type="password"
             className='modal_input'
             placeholder="Повторите пароль" />
+
+          {editPassMessage && (<p>{editPassMessage.message}</p>)}
+
           <button type='submit' className='btn modal_button' >сохранить</button>
         </form>
 
